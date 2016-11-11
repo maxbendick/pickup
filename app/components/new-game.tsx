e989d6212d057d139c9267d8825b2489662e0059
jcabre04 <cabrera.student@gmail.com>
2016-11-11 14:17:39 -0800

    Adding input fields to New Game

--- app/components/new-game.tsx
+++ app/components/new-game.tsx
@@ -1,20 +1,46 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
+import Game from "../models/game";
 
 export class NewGameProps {
-   public onForward;
    public onBack;
+   public submit: (game: Game) => void;
 }
+
+export class NewGameState {
+    public game: Game;
+}
+
+//constructor(public type: string, public distance: number, public time: number, 
+//public notes: string, players: Array<Player>)
 
 export default class NewGame extends Component<NewGameProps, null> {
    render() {
       return (
          <View>
             <Text>Current Scene: New Game</Text>
-            <TouchableHighlight onPress={this.props.onForward}>
-               <Text>Tap me to load the next scene</Text>
-            </TouchableHighlight>
+            <Text>Game:  </Text>
+            <TextInput
+                style={{height: 40, borderColor: 'black', borderWidth: 1}}
+                //onSubmitEditing={}
+            />
+            <Text>Time:  </Text>
+            <TextInput
+                style={{height: 40, borderColor: 'black', borderWidth: 1}}
+                //onSubmitEditing={}
+            />
+            <Text>Place:  </Text>
+            <TextInput
+                style={{height: 40, borderColor: 'black', borderWidth: 1}}
+                //onSubmitEditing={}
+            />
+            <Text>Description:  </Text>
+            <TextInput
+                style={{height: 40, borderColor: 'black', borderWidth: 1}}
+                //onSubmitEditing={}
+            />
+            
             <TouchableHighlight onPress={this.props.onBack}>
                <Text>Tap me to go back</Text>
             </TouchableHighlight>

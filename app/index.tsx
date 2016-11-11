5ba66b5a2dcdda99c503b70c8c835486088f311f
Max Bendick <maxbendick@gmail.com>
2016-11-11 12:00:52 -0800

    Navigation between screens (for dev)

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,92 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { StyleSheet, Text, View, Navigator } from 'react-native';
+import { StyleSheet, Text, View, Navigator, TouchableHighlight } from 'react-native';
 
 import GameDetail, { GameDetailProps } from './components/game-detail';
 import GameList, { GameListProps } from './components/game-list';
+import NewGame, { NewGameProps } from './components/new-game';
+import GameNotification, { GameNotificationProps } from './components/game-notification';
+
+export const enum ROUTES {
+   DEV_HOME, GAME_DETAIL, GAME_LIST, NEW_GAME, GAME_NOTIFICATION
+}
 
 export default class Index extends Component<null, null> {
    render() {
       return (
          <Navigator
-            initialRoute={{ title: 'My Initial Scene', index: 0 }}
-            renderScene={(route, navigator) =>
-               <GameDetail
+            initialRoute={{ title: 'My Initial Scene', index: ROUTES.DEV_HOME}}
+            renderScene={(route, navigator) => {
+
+               let onForward = () => {
+                  const nextIndex = route.index + 1;
+                  navigator.push({
+                     title: 'Scene ' + nextIndex,
+                     index: nextIndex,
+                  });
+               }
+               let onBack = () => {
+                  if (route.index > 0) {
+                     navigator.pop();
+                  }
+               }
+               let goTo = (title: string, index: number) => {
+                  navigator.push({title: title, index: index});
+               }
+
+               switch(route.index) {
+                  case ROUTES.DEV_HOME:
+                     return (
+                        <View>
+                           <Text>This is the Dev Homescreen</Text>
+                           <TouchableHighlight onPress={() =>  goTo("Game Detail", ROUTES.GAME_DETAIL)}>
+                              <Text>View Game Detail</Text>
+                           </TouchableHighlight>
+                           <TouchableHighlight onPress={() => goTo("Game List", ROUTES.GAME_LIST)}>
+                              <Text>View Game List</Text>
+                           </TouchableHighlight>
+                           <TouchableHighlight onPress={() => goTo("New Game", ROUTES.NEW_GAME)}>
+                              <Text>View New Game</Text>
+                           </TouchableHighlight>
+                           <TouchableHighlight onPress={() => goTo("Game Notification", ROUTES.GAME_NOTIFICATION)}>
+                              <Text>View Game Notification</Text>
+                           </TouchableHighlight>
+                        </View>
+                     );
+                  case ROUTES.GAME_DETAIL:
+                     return (
+                        <GameDetail
+                           gameName={route.title}       
+                           onForward={onForward}
+                           onBack={onBack}
+                           />
+                     );
+                  case ROUTES.GAME_LIST:
+                     return (
+                        <GameList      
+                           onForward={onForward}
+                           onBack={onBack}
+                           />
+                     );
+                  case ROUTES.NEW_GAME:
+                     return (
+                        <NewGame      
+                           onForward={onForward}
+                           onBack={onBack}
+                           />
+                     );
+                  case ROUTES.GAME_NOTIFICATION:
+                     return (
+                        <GameNotification      
+                           onForward={onForward}
+                           onBack={onBack}
+                           />
+                     );
+                  
+               }
+
+
+               return <GameDetail
                   gameName={route.title}
 
                   // Function to call when a new scene should be displayed           
@@ -30,6 +105,7 @@ export default class Index extends Component<null, null> {
                      }
                   } }
                   />
+               }
             }
             />
       );

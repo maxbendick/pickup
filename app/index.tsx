dad36fde8f515ab255ffe55747d724c04865292e
Max Bendick <maxbendick@gmail.com>
2016-11-11 17:12:41 -0800

    More games!

--- app/index.tsx
+++ app/index.tsx
@@ -45,8 +45,11 @@ export default class Index extends Component<null, IndexState> {
 
       let games = [
          new Game(basketball, 1, DAY_OF_WEEK.MON, "7 PM", "", players, chats[0]),
-         new Game(basketball, 1, DAY_OF_WEEK.MON, "7 PM", "", [players[0], players[1], players[3]], chats[0]),
-         new Game(baseball, 1, DAY_OF_WEEK.MON, "7 PM", "", [players[4], players[5], players[6]], chats[0])
+         new Game(basketball, 1, DAY_OF_WEEK.MON, "8 PM", "", [players[0], players[1], players[3]], chats[0]),
+         new Game(baseball, 1, DAY_OF_WEEK.MON, "7 PM", "", [players[4], players[5], players[6]], chats[0]),
+         new Game(basketball, 1, DAY_OF_WEEK.TUES, "11 AM", "", [players[1], players[4], players[5]], chats[0]),
+         new Game(baseball, 1, DAY_OF_WEEK.MON, "1 PM", "", [players[0], players[2], players[4], players[5]], chats[0]),
+         new Game(baseball, 1, DAY_OF_WEEK.MON, "1 PM", "", players, chats[0]),
       ];
 
       this.state = {

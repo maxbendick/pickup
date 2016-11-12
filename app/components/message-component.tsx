0fae65b6b76b0c77b996aefd7f0f40beb375529f
Max Bendick <maxbendick@gmail.com>
2016-11-11 19:52:20 -0800

    Messages look nice

--- app/components/message-component.tsx
+++ app/components/message-component.tsx
@@ -11,9 +11,31 @@ export class MessageProps {
 export default class MessageComponent extends Component<MessageProps, null> {
    render() {
       return (
-         <View>
-            <Text>{this.props.message.content}</Text>
+         <View style={styles.viewComp}>
+            <Text style={styles.container}>{this.props.message.content}</Text>
          </View>
       );
    }
 }
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#4080ff',
+      color: '#ffffff',
+      borderRadius: 10,
+      margin: 5,
+      marginRight: 5,
+      marginTop: 10,
+      padding: 10,
+      fontSize: 22,
+      maxWidth: 350
+      
+   } as React.ViewStyle & React.TextStyle,
+   text: {
+      fontSize: 10
+   } as React.TextStyle,
+   viewComp: {
+      alignItems: 'flex-end'
+   } as React.ViewStyle
+});

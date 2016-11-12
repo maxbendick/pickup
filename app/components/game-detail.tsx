2fb9675a9a5e9a06e67a711d7d75966f611b4ed1
Max Bendick <maxbendick@gmail.com>
2016-11-11 23:39:42 -0800

    Added better 'back' button

--- app/components/game-detail.tsx
+++ app/components/game-detail.tsx
@@ -49,7 +49,7 @@ export default class GameDetail extends Component<GameDetailProps, State> {
          <View style={styles.container}>
             <View style={styles.navContainer}>
                <TouchableHighlight onPress={this.props.onBack}>
-                  <Text style={styles.backBtn}>&lt;</Text>
+                  <Image style={{height: 25, width: 25}} source={require('../../images/Back-50.png')}/>
                </TouchableHighlight>
                <Text style={styles.headerText}>{this.props.game.type.type} Game</Text>
                <TouchableHighlight>
